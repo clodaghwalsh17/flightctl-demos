73ad06db550be733f14ea62166f1a5d46dc41062
Clodagh Walsh <clodaghwalsh17@gmail.com>
2026-04-22 09:31:54 +0100

    Remove unnecessary workspace and change the model to be a Red Hat provided model
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- demos/mlops-inference-stack-demo/apps/modelcar/model-downloader.py
+++ demos/mlops-inference-stack-demo/apps/modelcar/model-downloader.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 # Specify the Hugging Face repository containing the model
-model_repo = "meta-llama/Llama-3.2-1B-Instruct"
+model_repo = "RedHatAI/Llama-3.2-1B-Instruct-quantized.w8a8"
 snapshot_download(
     repo_id=model_repo,
     local_dir="/models",
